32a966005183ed2dd71ab23d7f0e4b46d3fee2c5
Vepsar1337 <vepsar1337@mail.ru>
2016-09-27 02:15:35 +0300

    123
    321

---
+++ src/by/bsuir/misoi/filter/GrayScale.java
@@ -0,0 +1,53 @@
+package by.bsuir.misoi.filter;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
+
+public class GrayScale implements IFilter 
+{
+	BufferedImage image;
+	int width;
+	int height;
+	
+	public GrayScale() 
+	{
+	}
+
+	@Override
+	public void doFilter(BufferedImage original)
+	{
+		try 
+		{
+			image = original;
+			width = image.getWidth();
+			height = image.getHeight();
+			
+			for(int i=0; i<height; i++)
+			{
+				for(int j=0; j<width; j++)
+				{
+					Color c = new Color(image.getRGB(j, i));
+					
+					int red = (int)(c.getRed() * 0.299);
+					int green = (int)(c.getGreen() * 0.587);
+					int blue = (int)(c.getBlue() *0.114);
+					
+					Color newColor = new Color(red+green+blue,red+green+blue,red+green+blue);
+					image.setRGB(j,i,newColor.getRGB());
+				}
+			}
+			
+			File ouptut = new File("GrayScale.jpg");
+			ImageIO.write(image, "jpg", ouptut);
+			System.out.print("GRAY SDELALSYA !!!! kartinka sohranena v GrayScale.jpg");
+		} catch (Exception e) 
+		{
+			
+		}
+		
+	}
+
+}
