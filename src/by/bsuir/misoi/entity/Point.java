162b383f2afcba628f1f4d597bac7bd74c355f9a
Vepsar1337 <vepsar1337@mail.ru>
2016-12-20 11:41:57 +0300

    ...

--- src/by/bsuir/misoi/entity/Point.java
+++ src/by/bsuir/misoi/entity/Point.java
@@ -6,4 +6,11 @@ package by.bsuir.misoi.entity;
 public class Point {
     public int x;
     public int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point (){};
 }
